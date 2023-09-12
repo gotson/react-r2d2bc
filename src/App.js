c848c6e9f10285f66d3a3c0462512962b0c99224
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2023-09-12 13:21:43 +0800

    add r2d2bc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import D2Reader from '@d-i-t-a/reader'
+
+D2Reader.load({
+  url: new URL("http://localhost:15080/QSBDbGFzaCBvZiBLaW5ncyAtIEdlb3JnZSBSLiBSLiBNYXJ0aW4uZXB1Yg/manifest.json"),
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="D2Reader-Container">
+      <main style={{height: "100vh", tabindex: -1}} id="iframe-wrapper"/>
+
     </div>
   );
 }
